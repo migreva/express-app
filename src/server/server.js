a744a5dbe56b933aa5591dba80735cffa481cd87
migreva <mikevee313@gmail.com>
2015-10-25 20:33:42 -0400

    added debug

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,14 @@
+import debug from 'debug';
+
 import app from './app';
+
+let logger = debug('server');
 
 let server = app.listen(3000, function () {
 
   let host = server.address().address;
   let port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  logger('Example app listening at http://%s:%s', host, port);
 
 });
