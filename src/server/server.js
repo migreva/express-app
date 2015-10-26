e6a405d472f8054f17a673436b720e220c0721e7
migreva <mikevee313@gmail.com>
2015-10-25 22:48:25 -0400

    nginx conf

--- src/server/server.js
+++ src/server/server.js
@@ -4,7 +4,7 @@ import app from './app';
 
 let logger = debug('server');
 
-let server = app.listen(3000, function () {
+let server = app.listen(3003, function () {
 
   let host = server.address().address;
   let port = server.address().port;
